949b58f5f10a2d92bc9722d100b862404322db05
Diego Pienda <412672@tecnicatura.frc.utn.edu.ar>
2024-10-02 18:23:04 -0300

    .

---
+++ ActividadPractica 05/ActividadPractica 05/Repositorio/TurnoRepositorio.cs
@@ -0,0 +1,52 @@
+﻿using ActividadPractica_05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadPractica_05.Repositorio
+{
+    public interface ITurnoRepositorio
+    {
+        bool AgregarTurno(T_TURNO turno);
+        bool ModificarTurno(T_TURNO turno);
+        bool EliminarTurno(int id);
+        List<T_TURNO> ObtenerTurnos();
+    }
+
+    public class TurnoRepositorio : ITurnoRepositorio
+    {
+        private readonly db_turnosContext _context;
+        public TurnoRepositorio(db_turnosContext db_TurnosContext)
+        {
+            _context = db_TurnosContext;
+        }
+
+        public bool AgregarTurno(T_TURNO turno)
+        {
+            _context.T_TURNOs.Add(turno);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool EliminarTurno(int id)
+        {
+            _context.T_TURNOs.Remove(_context.T_TURNOs.Find(id));
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool ModificarTurno(T_TURNO turno)
+        {
+            _context.T_TURNOs.Update(turno);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public List<T_TURNO> ObtenerTurnos()
+        {
+            return _context.T_TURNOs.ToList();
+        }
+    }
+}
---
+++ ActividadPractica 05/WebApplicationTurno/Controllers/TurnoControlle.cs
@@ -0,0 +1,77 @@
+﻿using ActividadPractica_05.Models;
+using ActividadPractica_05.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace WebApplicationTurno.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurnoControlle : ControllerBase
+    {
+        private readonly ITurnoRepositorio _turnoRepositorio;
+        public TurnoControlle(ITurnoRepositorio turnoRepositorio)
+        {
+            _turnoRepositorio = turnoRepositorio;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_turnoRepositorio.ObtenerTurnos());
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] T_TURNO turno)
+        {
+            
+            DateTime FechaMinima = DateTime.Now.AddDays(1);
+            DateTime FechaMaxima = DateTime.Now.AddDays(45);
+            
+            if (DateTime.TryParse(turno.fecha, out DateTime fecha))
+            {
+                if(fecha >= FechaMinima && fecha <= FechaMaxima)
+                {
+                    var TurnoExistenstes = _turnoRepositorio.ObtenerTurnos();
+
+                    if (TurnoExistenstes.Any(t => t.fecha == turno.fecha && t.hora == turno.hora))
+                    {
+                        return BadRequest("Ya existe un turno con la misma fecha y hora");
+                    }
+
+                    var servicios = new HashSet<int>();
+                    foreach (var detalle in turno.Detalles)
+                    {
+                        if (!servicios.Add(detalle.id_servicio))
+                        {
+                            return BadRequest("No se pueden repetir los servicios");
+                        }
+                    }
+                    if (turno.Detalles.Count == 0)
+                    {
+                        return BadRequest("El turno debe tener al menos un detalle");
+                    }
+                    if (_turnoRepositorio.AgregarTurno(turno))
+                    {
+                        return Ok("Se Agrego correctamente"+ " " +turno);
+                    }
+                    else
+                    {
+                        return BadRequest("Error al agregar el turno");
+                    }
+                }
+                else
+                {
+                    return BadRequest("La fecha debe ser mayor a la fecha actual y menor a 45 dias");
+                }
+            }
+            else
+            {
+                return BadRequest("La fecha no es valida");
+            }
+           
+
+        }
+
+
+    }
+}
